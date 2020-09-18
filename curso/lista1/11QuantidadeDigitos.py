11000d12faf652bc8da93a759b606c370b4af0d4
Ana Araujo <anapro1141@gmail.com>
2020-09-18 18:24:12 -0300

    arrumei endentacao ultimo exercicio

--- curso/lista1/11QuantidadeDigitos.py
+++ curso/lista1/11QuantidadeDigitos.py
@@ -1,4 +1,5 @@
 ''' Sabendo que str( ) converte valores numéricos para string, calcule quantos dígitos há em 2 elevado
 a um milhão.'''
+
 print(' === QUANTIDADE DE DIGÍTOS === ')
 print (f'A quantidade de digítos é: {len(str(2 ** 1000000))}')
