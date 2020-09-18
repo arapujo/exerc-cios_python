57cfffa73856a58fe154e42255a2409e86257bc7
Ana Araujo <anapro1141@gmail.com>
2020-09-18 19:31:47 -0300

    adicionei espaco

--- python_brasil/feminino_masculino.py
+++ python_brasil/feminino_masculino.py
@@ -1,4 +1,5 @@
 ''' Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. '''
+
 print('=== FEMININO OU MASCULINO ===')
 letra = input('Digite uma letra: ')
 letra_comparacao = letra.upper()
--- python_brasil/negativo_positivo.py
+++ python_brasil/negativo_positivo.py
@@ -1,4 +1,5 @@
 ''' Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. '''
+
 print('=== POSITIVO OU NEGATIVO ===')
 n = float(input('Insira um número: '))
 if n >= 0:
