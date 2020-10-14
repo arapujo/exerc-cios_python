23306deed917944d07a9ea9fb059052c539a60e7
Ana Araujo <anapro1141@gmail.com>
2020-10-14 17:45:23 -0300

    exercicios lista 7

--- pyhton_para_zumbis/lista7/coding_bat7.py
+++ pyhton_para_zumbis/lista7/coding_bat7.py
@@ -9,3 +9,46 @@ def multstring(s, n):
         return print(s*n)
 
 multstring('Hi', 2)
+
+# B. string_splosion
+# string_splosion('Code') -> 'CCoCodCode'
+# string_splosion('abc') -> 'aababc'
+# string_splosion('ab') -> 'aab'
+def string_splosion(s):
+    string = ''
+    for i in range(len(s)):
+        string += s[:i+1]
+    return string
+
+print(string_splosion('Code'))
+print(string_splosion('abc'))
+
+# C. array_count9
+# conta quantas vezes aparece o 9 numa lista nums
+def array_count9(nums):
+  return nums.count(9)
+
+print(array_count9([1, 2, 3, 9, 9]))
+
+# D. array_front9
+# verifica se pelo menos um dos quatro primeiros é nove
+# array_front9([1, 2, 9, 3, 4]) -> True
+# array_front9([1, 2, 3, 4, 9]) -> False
+# array_front9([1, 2, 3, 4, 5]) -> False
+def array_front9(nums):
+  if nums[0:5].count(9) != 0:
+    return True
+  else:
+    return False
+print(array_front9([1, 2, 3, 9, 9]))
+print(array_front9([8, 7, 6, 5]))
+
+# E. hello_name
+# seja uma string name
+# hello_name('Bob') -> 'Hello Bob!'
+# hello_name('Alice') -> 'Hello Alice!'
+# hello_name('X') -> 'Hello X!'
+def hello_name(name):
+  return print(f'Hello {name}')
+
+hello_name('Bob')
