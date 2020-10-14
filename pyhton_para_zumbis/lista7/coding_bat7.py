e74e19b9d19d1b902f91eb194f4f167779e735bb
Ana Araujo <anapro1141@gmail.com>
2020-10-14 15:08:32 -0300

    exericicio a lista 7

---
+++ pyhton_para_zumbis/lista7/coding_bat7.py
@@ -0,0 +1,11 @@
+# Exercícios by Nick Parlante (CodingBat)
+
+# A. multstring
+# seja uma string s e um inteiro positivo n
+# retorna uma string com n cópias da string original
+# multstring('Hi', 2) -> 'HiHi'
+def multstring(s, n):
+    if s and n >= 0:
+        return print(s*n)
+
+multstring('Hi', 2)
