3d578cf629be1fd9ee3b24555b948e9399f7c150
Ana Araujo <anapro1141@gmail.com>
2020-10-20 11:33:01 -0300

    exercicio g lista9

--- pyhton_para_zumbis/lista9/coding_bat9.py
+++ pyhton_para_zumbis/lista9/coding_bat9.py
@@ -79,3 +79,31 @@ def middle_way(a, b):
     meio_b = len(b) // 2
     nova_lista = [a[meio_a], b[meio_b]]
     return nova_lista
+
+
+# G. date_fashion
+# você e sua namorada(o) vão a um restaurante
+# eu e par são as notas das suas roupas de 0 a 10
+# quanto maior a nota mais chique vocês estão vestidos
+# o resultado é se vocês conseguiram uma mesa no restaurante:
+# 0=não 1=talvez e 2=sim
+# se a nota da roupa de um dos dois for menor ou igual a 2
+# vocês não terão direito à uma mesa (0)
+# se as notas são maiores, então caso um dos dois esteja
+# bem chique (nota >= 8) então a resposta é sim (2)
+# caso contrário a resposta é talvez (1)
+# date_fashion(5, 10) -> 2
+# date_fashion(5, 2) -> 0
+# date_fashion(5, 5) -> 1
+
+def date_fashion(eu, par):
+    if eu <= 2 or par <=2:
+        return 0
+    elif eu >= 8 or par >= 8:
+        return 2
+    else:
+        return 1
+
+print(date_fashion(5, 10))
+print(date_fashion(5, 2))
+print( date_fashion(5, 5))
