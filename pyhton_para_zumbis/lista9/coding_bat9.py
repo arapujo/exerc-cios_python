81f29b5482f36fa3a0c31b27485e2ffdb5dbe497
Ana Araujo <anapro1141@gmail.com>
2020-10-20 10:52:51 -0300

    exercicio b lista9

--- pyhton_para_zumbis/lista9/coding_bat9.py
+++ pyhton_para_zumbis/lista9/coding_bat9.py
@@ -12,4 +12,19 @@ def first_last6(nums): #
     else:
         return False
 
+# B. same_first_last #
+# retorna True se a lista nums possui pelo menos um elemento
+# e o primeiro elemento é igual ao último
+# same_first_last([1, 2, 3]) -> False
+# same_first_last([1, 2, 3, 1]) -> True
+# same_first_last([1, 2, 1]) -> True
+
+def same_first_last(nums):
+    if len(nums) > 0 and nums[0] == nums[-1]:
+        return True
+    else:
+        return False
+
+print(same_first_last([1, 2, 3]))
+print(same_first_last([1, 2, 3, 1]))
 
